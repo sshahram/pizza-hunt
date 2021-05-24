08bc9f0d8b3072236b5d00f791d70fed24370ad7
sshahram <shirin.shahram23@gmail.com>
2021-05-24 15:42:34 -0700

    create a method to update a pizza

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -35,6 +35,19 @@ const pizzaController = {
             .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => res.status(400).json(err));
     },
+
+    // update pizza by id
+    updatePizza({params, body}, res) {
+        Pizza.findOneAndUpdate({_id: params.id}, body, {new: true})
+            .then(dbPizzaData => {
+                if(!dbPizzaData) {
+                    res.status(404).json({message: 'No pizza found with this id!'});
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
 };
 
 module.exports = pizzaController;
