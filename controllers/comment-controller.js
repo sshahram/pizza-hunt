034077119eb1a170f7b96d521350647e610fa096
sshahram <shirin.shahram23@gmail.com>
2021-05-25 16:17:58 -0700

    create a method to add a reply

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -22,6 +22,23 @@ const commentController = {
             .catch(err => res.json(err));
     },
 
+    // add reply to comment
+    addReply({params, body}, res) {
+        Comment.findOneAndUpdate(
+            {_id: params.commentId},
+            {$push: {replies: body}},
+            {new: true}
+        )
+            .then(dbPizzaData => {
+                if(!dbPizzaData) {
+                    res.status(404).json({message: 'No pizza found with this id!'});
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.json(err));
+    },
+
     // remove comment
     removeComment({ params }, res) {
         Comment.findOneAndDelete({ _id: params.commentId })
@@ -43,7 +60,7 @@ const commentController = {
                 res.json(dbPizzaData);
             })
             .catch(err => res.json(err));
-    }
+    },
 };
 
 module.exports = commentController;
