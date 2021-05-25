3ea8949079f4fd27f7f28976281b1a11f9d88c90
sshahram <shirin.shahram23@gmail.com>
2021-05-25 15:21:57 -0700

    create associations between reply and comment

--- models/Comment.js
+++ models/Comment.js
@@ -1,9 +1,9 @@
-const {Schema, model, Types} = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
 const ReplySchema = new Schema(
     {
-        // set custome if to avoid confusion with parent comment _id
+        // set custome id to avoid confusion with parent comment _id field
         replyId: {
             type: Schema.Types.ObjectId,
             default: () => new Types.ObjectId()
@@ -19,21 +19,38 @@ const ReplySchema = new Schema(
             default: Date.now,
             get: createdAtVal => dateFormat(createdAtVal)
         }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
     }
 );
 
-const CommentSchema = new Schema({
-    writtenBy: {
-        type: String
+const CommentSchema = new Schema(
+    {
+        writtenBy: {
+            type: String
+        },
+        commentBody: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        // use ReplySchema to validate data for a reply
+        replies: [ReplySchema]
     },
-    commentBody: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
     }
-});
+);
 
 const Comment = model('Comment', CommentSchema);
 
