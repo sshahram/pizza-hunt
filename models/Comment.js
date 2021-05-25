bb2b33184ec869ffb2af51a26b17d30d3b1ac5d0
sshahram <shirin.shahram23@gmail.com>
2021-05-25 14:14:54 -0700

    create ReplySchema

--- models/Comment.js
+++ models/Comment.js
@@ -1,4 +1,26 @@
-const {Schema, model} = require('mongoose');
+const {Schema, model, Types} = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ReplySchema = new Schema(
+    {
+        // set custome if to avoid confusion with parent comment _id
+        replyId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        replyBody: {
+            type: String
+        },
+        writtenBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    }
+);
 
 const CommentSchema = new Schema({
     writtenBy: {
