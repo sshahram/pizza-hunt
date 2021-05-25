3d2085c6054974bbcbe9c1385aaa2bef83bbecbb
sshahram <shirin.shahram23@gmail.com>
2021-05-24 23:16:15 -0700

    create a method to add comments

---
+++ controllers/comment-controller.js
@@ -0,0 +1,31 @@
+const {Comment, Pizza} = require('../models');
+
+const commentController = {
+    // add comment to pizza
+    addComment({params, body}, res) {
+        console.log(body);
+        Comment.create(body)
+            .then(({_id}) => {
+                return Pizza.findOneAndUpdate(
+                    {_id: params.pizzaId},
+                    {$push: {comments:_id}},
+                    {new: true}
+                );
+            })
+            .then(dbPizzaData => {
+                if(!dbPizzaData) {
+                    res.status(404).json({message: 'No pizza found with this id!'});
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.json(err));
+    },
+
+    // remove comment
+    removeComment() {
+
+    }
+};
+
+module.exports = commentController;
