217ae97fd7f18d398393cec55741d639289949d4
sshahram <shirin.shahram23@gmail.com>
2021-05-26 13:51:57 -0700

    save pizza data to IndexedDB

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -27,3 +27,15 @@ request.onerror = function(event) {
     // log error here
     console.log(event.target.errorCode);
 };
+
+// this function will be executed if we attempt to submit a new pizza and there's no internet connection
+function saveRecord(record) {
+    // open a new transaction with the database with read and write permissions
+    const transaction = db.transaction(['new_pizza'], 'readwrite');
+
+    // access the object store for `new_pizza`
+    const pizzaObjectStore = transaction.objectStore('new_pizza');
+
+    // add record to your store with add model
+    pizzaObjectStore.add(record);
+}
