e524c9fe44cce712635781ffc688539ee8101fd7
sshahram <shirin.shahram23@gmail.com>
2021-05-26 21:14:58 -0700

    remove alert from adding pizza

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -63,7 +63,7 @@ const handlePizzaSubmit = event => {
   })
   .then(response => response.json())
   .then(postResponse => {
-    alert('Pizza created successfully!');
+    // alert('Pizza created successfully!');
     console.log(postResponse);
   })
   .catch(err => {
