764adb20b2792182abc766b567ce623a5b4c53ef
sshahram <shirin.shahram23@gmail.com>
2021-05-26 19:57:51 -0700

    add validation to pizzaName and createdBy

--- models/Pizza.js
+++ models/Pizza.js
@@ -4,10 +4,14 @@ const dateFormat = require('../utils/dateFormat');
 const PizzaSchema = new Schema(
     {
         pizzaName: {
-            type: String
+            type: String,
+            required: 'You need to provide a pizza name!',
+            trim: true
         },
         createdBy: {
-            type: String
+            type: String,
+            required: true,
+            trim: true
         },
         createdAt: {
             type: Date,
